13bf47a2adec83f6e47bb350ab439c5c7d18935c
adm_mmojica <adm_mmojica@CE-IT01-SP>
2018-06-18 09:36:51 -0600

    Working DBcontext

---
+++ webEFCodeFirst/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webEFCodeFirst.Models
+{
+    public class Cliente
+    {
+        public int clienteId { get; set; }
+        public string Nombre { get; set; }
+
+        public TipoCliente Tipo { get; set; }
+
+    }
+}
---
+++ webEFCodeFirst/Models/contextoAplicacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace webEFCodeFirst.Models
+{
+    public class contextoAplicacion: DbContext
+    {
+        public contextoAplicacion():base("name=DefaultConnectionString")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<TipoCliente> Tipos { get; set; }
+
+    }
+}
