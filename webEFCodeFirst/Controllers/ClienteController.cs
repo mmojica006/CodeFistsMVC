675b78db57610c33076e6721ce2572ead3b3daef
adm_mmojica <adm_mmojica@CE-IT01-SP>
2018-06-20 14:23:20 -0600

    Agregando create controlador de Cliente

---
+++ webEFCodeFirst/Controllers/ClienteController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webEFCodeFirst.Models;
+
+namespace webEFCodeFirst.Controllers
+{
+    public class ClienteController : Controller
+    {
+       private  contextoAplicacion db = new contextoAplicacion();
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var consulta = from c in db.Clientes
+                           select new
+                           {
+                               id = c.clienteId,
+                               Mombrecliente = c.Nombre,
+                               Tipo = c.Tipo.NombreTipo
+
+                           };
+
+            return Json(consulta.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public string Crear(int idTipo, string nombreCliente)
+        {
+            if ( nombreCliente != null)
+            {
+                var tipoCliente = db.Tipos.Find(idTipo);
+
+                if (tipoCliente != null)
+                {
+                    Cliente cliente = new Cliente();
+                    cliente.Nombre = nombreCliente;
+                    cliente.Tipo = tipoCliente;
+                    db.Clientes.Add(cliente);
+                    db.SaveChanges();
+                    return "El registro se ha agregado";
+
+                }else
+                {
+                    return "El tipo de cliente no se encontro";
+                }
+
+            }else
+            {
+                return "No se proporciono el nombre del cliente";
+            }
+
+        }
+    }
+}
--- webEFCodeFirst/Controllers/TipoClienteController.cs
+++ webEFCodeFirst/Controllers/TipoClienteController.cs
@@ -1,17 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using webEFCodeFirst.Models;
 
 namespace webEFCodeFirst.Controllers
 {
     public class TipoClienteController : Controller
     {
+       private  contextoAplicacion dbContext = new contextoAplicacion();
+
         // GET: TipoCliente
         public ActionResult Index()
         {
-            return View();
+            return Json(dbContext.Tipos.ToList(),JsonRequestBehavior.AllowGet);
         }
+        public string Crear(string nombreTipo)
+        {
+
+            if (nombreTipo != null)
+            {
+                TipoCliente tipoCliente = new TipoCliente();
+                tipoCliente.NombreTipo = nombreTipo;
+                dbContext.Tipos.Add(tipoCliente);
+                dbContext.SaveChanges();
+
+                return "Se agrgo el registro de manera correcta";
+
+
+            }else
+            {
+                return "El nombre del tipo no es correcto";
+            }
+
+
+        }
+
+        public string Modificar (int id, string nombreTipo)
+        {
+            if (nombreTipo != null)
+            {
+                TipoCliente tipocliente = dbContext.Tipos.Find(id);
+                if (tipocliente != null)
+                {
+                    tipocliente.NombreTipo = nombreTipo;
+                    dbContext.Entry(tipocliente).State = EntityState.Modified;
+                    dbContext.SaveChanges();
+
+                    return ("El registro se modifico correctamente");
+
+
+                }else
+                {
+                    return ("El tipo no se encuentra");
+                }
+
+            }else
+            {
+                return "No se espeficico de manera correcta";
+            }
+
+        }
+
+        public string Eliminar(int id)
+        {
+            TipoCliente tipocliente = dbContext.Tipos.Find(id);
+
+            if (tipocliente != null)
+            {
+                dbContext.Tipos.Remove(tipocliente);
+                dbContext.SaveChanges();
+                return "El registro se ha eliminado correctamente";
+            }else
+            {
+                return "El tipo indicado no se encuentra";
+            }
+
+        } 
+
     }
 }
